a7a95e4e8d54164cd7dba6774b7490df68c174c9
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-01-19 16:52:41 -0600

    began experimenting with calculator based on lesson 2 slides

--- blank.js
+++ blank.js
@@ -1,1 +1,27 @@
 console.log('hi');
+
+let x = 'woof woof';
+let y = 'pet me';
+
+if (30 / 3 === 11) {
+  console.log(x);
+  } else if (x === 'pet me') {
+    console.log(y);
+  } else if (y === 'woof woof') {
+    console.log('treats');
+  }
+
+
+/*This Regular Challenge homework is required.
+
+Fork and clone the class repository for assignments: https://github.com/UWFront-End-Cert/html200-coding
+
+Follow good file organization and naming conventions. For each lesson, create a directory appropriately labeled for that lesson. E.g. for this week, create a  Lesson02 directory in your local repository.
+
+Write a program called calculator.js. We've demonstrated writing code using CodePen. Feel free to use this tool or another that you are comfortable with.
+
+Declare at least 4 variables, and then do math operations on them. Mix and match operators and variables and console log the results of these operations.
+
+Save your work to the Lesson02 directory.
+
+Make a pull request to the parent directory.*/
