35c745ca4935d14de432d7c9d239266227dd5ead
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-01-20 14:56:52 -0600

    changed var to let, added comment after assignment

--- lesson02.js
+++ lesson02.js
@@ -1,5 +1,5 @@
-var x = 'woof woof';
-var y = 'pet me';
+let x = 'woof woof';
+let y = 'pet me';
 if ( 15 / 5 === 5) {
   console.log(x);
   } else if (x === 'pet me') {
@@ -19,19 +19,13 @@ if ('payFee') {
   alert(x);
 }
 
-
+/*assuming the 404 error is happening on GitHub Pages because I have no HTML*/
 
 
 /*This Regular Challenge homework is required.
-
 Fork and clone the class repository for assignments: https://github.com/UWFront-End-Cert/html200-coding
-
 Follow good file organization and naming conventions. For each lesson, create a directory appropriately labeled for that lesson. E.g. for this week, create a  Lesson02 directory in your local repository.
-
 Write a program called calculator.js. We've demonstrated writing code using CodePen. Feel free to use this tool or another that you are comfortable with.
-
 Declare at least 4 variables, and then do math operations on them. Mix and match operators and variables and console log the results of these operations.
-
 Save your work to the Lesson02 directory.
-
 Make a pull request to the parent directory.*/
