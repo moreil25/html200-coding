63a04815a5c8201b16f72809b557d53013657483
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-01-24 11:36:25 -0600

    created variables and performed math operations using console.log

--- lesson02.js
+++ lesson02.js
@@ -1,23 +1,12 @@
-let var1 =
-let var2 =
-let var3 =
-let var4 =
+let var1 = 3;
+let var2 = 2;
+let var3 = 6;
+let var4 = 0;
 
-
-/*For this assignment, you don't need to build a web page that contains a calculator where the user can press buttons and such. Instead, you just have to make a Javascript program that can be called from the Javascript console and used to perform basic calculator functions, like add/subtract/multiply/divide. Of course, Javascript itself should be able to perform the heavy lifting of the math calculations, so you just have to call the built-in functions accordingly. Does that make sense?*/
-
-
-
-
-
-
-/*assuming the 404 error is happening on GitHub Pages because I have no HTML*/
-
-
-/*This Regular Challenge homework is required.
-Fork and clone the class repository for assignments: https://github.com/UWFront-End-Cert/html200-coding
-Follow good file organization and naming conventions. For each lesson, create a directory appropriately labeled for that lesson. E.g. for this week, create a  Lesson02 directory in your local repository.
-Write a program called calculator.js. We've demonstrated writing code using CodePen. Feel free to use this tool or another that you are comfortable with.
-Declare at least 4 variables, and then do math operations on them. Mix and match operators and variables and console log the results of these operations.
-Save your work to the Lesson02 directory.
-Make a pull request to the parent directory.*/
+console.log(var1 + var3);
+console.log(var2 - var4);
+console.log((var1 * var2) + var3);
+console.log(var4 * (var3 / var1));
+console.log(var3 % var2);
+console.log(var2 ** 2);
+console.log((var1 / var4) ** 50 + 1);
