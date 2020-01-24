fd4a2aee9c1e84f3f91fb1f8664139afd1666ab6
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-01-24 11:50:11 -0600

    renamed file to calculator.js per assignment requirement

---
+++ calculator.js
@@ -0,0 +1,12 @@
+let var1 = 3;
+let var2 = 2;
+let var3 = 6;
+let var4 = 0;
+
+console.log(var1 + var3);
+console.log(var2 - var4);
+console.log((var1 * var2) + var3);
+console.log(var4 * (var3 / var1));
+console.log(var3 % var2);
+console.log(var2 ** 2);
+console.log((var1 / var4) ** 50 + 1);
