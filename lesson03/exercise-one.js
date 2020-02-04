b2bbf378450f9edfad185e068757787d9d31b646
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-02-04 10:03:35 -0500

    completed exercises one through three

--- lesson03/exercise-one.js
+++ lesson03/exercise-one.js
@@ -1,15 +1,3 @@
-/*This Regular Challenge homework is required.
-
-Create a JavaScript file that does the following:
-
-Do a console log after each item
-
-Create an array, with at least 5 strings
-Add an element to the end of the end of the array
-Remove the third element
-Create a string from the elements and comma separate them*/
-
-
 let arr = ['book', 'tablet', 'kindle', 'phone', 'laptop'];
 
 arr.push('audio');
--- lesson03/exercise-three.js
+++ lesson03/exercise-three.js
@@ -1,21 +1,7 @@
-/*Create an array that has at least 5 strings:
-
-Use a for loop to console log all of the elements plus the index and a comma
-Example:
-
-// 1,cats
-// 2,dogs
-
-..and so on*/
-
-
 let arr = ['carrot','cucumber','broccoli','brussel sprouts','cabbage'];
 
 let text = ",";
 
-
 for (let i = 0; i < arr.length; i++) {
-  console.log(index + text + arr);
+  console.log(i + text + arr[i]);
 }
-
-//Uncaught ReferenceError: index is not defined at pen.js:1
