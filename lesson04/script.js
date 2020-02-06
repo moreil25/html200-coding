c08a7717f18ceb7c546fe5c0a32aef31ec243ac6
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-02-06 11:10:53 -0500

    added two functions and an event handler to the documents

---
+++ lesson04/script.js
@@ -0,0 +1,16 @@
+function numberSquared() {
+  let input = prompt('Enter a number');
+  let result = 0;
+
+  result = Number(input) **2;
+    alert(result);
+}
+
+function myMultiplier() {
+  let inputTwo = prompt('Enter a number again');
+  let inputThree = prompt('Enter one more number');
+  let resultTwo = (inputTwo, inputThree);
+
+  resultTwo = Number(inputTwo) * Number(inputThree);
+    alert(resultTwo);
+}
