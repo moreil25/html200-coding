c1d33b3c19a8a06fc1835d0037e2a64b0c342b8c
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-03-04 16:53:29 -0600

    reworked if/else statements

--- lesson05/bankApplication.js
+++ lesson05/bankApplication.js
@@ -1,18 +1,14 @@
 let myBalance = 300;
 
-
-//Ask for input using a dialog box, then display input in a dialog box -- trying to figure out how/where this comes into play (last slide of Lesson 5), Jeremy mentioned it during office hours
-
 function launchApp() {
   let bankMode = prompt('Please select from the following options: \n W \n D \n B \n Q');
-  if (bankMode == 'W') {
+  firstInput = bankMode;
+  if (firstInput == 'W') {
     makeWithdrawal();
-  } else if (bankMode == 'D') {
+  } else if (firstInput == 'D') {
     makeDeposit();
-  } else if (bankMode == 'B') {
-    myBalance();
-  // } if (bankMode === 'Q') {
-  //   break;
+  } else if (firstInput == 'B') {
+    checkBalance();
   }
 }
 
@@ -21,7 +17,7 @@ function makeWithdrawal() {
   let result = 0;
   result = myBalance - Number(input);
   myBalance = result;
-  alert(result);
+  alert('Your balance is' + ' $' + result);
 }
 
 function makeDeposit() {
@@ -29,16 +25,9 @@ function makeDeposit() {
   let result = 0;
   result = myBalance + Number(input);
   myBalance = result;
-  alert(result);
+  alert('Your balance is' + ' $' + result);
 }
 
 function checkBalance() {
-  alert(myBalance);
+  alert('Your balance is' + ' $' + myBalance);
 }
-
-//Extra challenges: The additional requirements below are optional. Implement them if you finish the Regular Challenge 1 (above) and are up for more!
-  //The user should not be able to make excessive withdrawals (no negative balances).
-  //The user should have a deposit cap of $50,000.
-  //The user should receive a warning to protect against low balances.
-  //Create an alert if the user tries to withdraw an amount that would leave them with less than $300 in their account, asking them to confirm if they're sure.
-  //Publish your work to github.io
